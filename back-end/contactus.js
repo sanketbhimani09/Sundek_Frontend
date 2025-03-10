51f3c62feeac1325287ee71446bae9e7b30def34
Sanket Bhimani <117342649+sanketbhimani09@users.noreply.github.com>
2025-03-10 19:22:59 +0530

    Delete back-end directory

--- back-end/contactus.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const contactSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    message:String
-})
-const ContactModel = mongoose.model("messages", contactSchema)
-module.exports=ContactModel
